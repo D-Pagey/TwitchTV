40adf0210c7a967d65ec3ad260fdf627071e1d84
Pagey <dan.page91@gmail.com>
2017-12-25 02:05:12 +0000

    Append link to status.

--- scripts.js
+++ scripts.js
@@ -1,12 +1,13 @@
 const url = "https://wind-bow.glitch.me/twitch-api/streams/";
-const user = ["freecodecamp", "yo_boy_roy", "rainbow6", "kixstar", "shroud", "break"];
+const user = ["freecodecamp", "yo_boy_roy", "rainbow6", "kixstar", "shroud", "cdnthe3rd"];
 const urlEnd = "?callback=";
-// const status = document.getElementsByClassName("stream-container");
-//
-// console.log(status[0]);
 
 function extractJson(data) {
   return data.json();
+}
+
+function createLink() {
+
 }
 
 user.forEach(function(element) {
@@ -25,6 +26,15 @@ const finalUrl = url + element + urlEnd;
         stream.innerHTML = "Online";
         let status = document.getElementById(data.stream.channel.name + "-stream");
         status.innerHTML = data.stream.channel.status;
+
+        const link = document.createElement('a');
+        link.href = "https://www.twitch.tv/" + data.stream.channel.name;
+        link.target = "_blank";
+        const snippet = document.createTextNode("testing the link");
+        link.appendChild(snippet);
+        const cdnDiv = document.getElementById("cdnDiv")
+        cdnDiv.appendChild(link);
+        link.appendChild(status);
 
       }
     })
