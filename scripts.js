96727b8012b81c4de794a350ae78e2bb833fbd22
Pagey <dan.page91@gmail.com>
2017-12-26 22:03:39 +0000

    Stuck on JS, started styling for mobile.

--- scripts.js
+++ scripts.js
@@ -1,14 +1,36 @@
 const url = "https://wind-bow.glitch.me/twitch-api/streams/";
-const user = ["freecodecamp", "yo_boy_roy", "rainbow6", "kixstar", "shroud", "cdnthe3rd"];
+const user = ["freecodecamp", "yo_boy_roy", "silentsentry", "shroud", "macie_jay", "scream", "symfuhny"];
 const urlEnd = "?callback=";
 
 function extractJson(data) {
   return data.json();
 }
 
-function createLink() {
+function createLink(data) {
+  const stream = document.getElementById(data.stream.channel.name);
+  const status = document.getElementById(data.stream.channel.name + "-stream");
+  const div = document.getElementById(data.stream.channel.name + "-div")
 
+  if (data.stream != null) {
+    console.log("online");
+
+    stream.className = "stream-status--online";
+    stream.innerHTML = "Online";
+
+    const link = document.createElement('a');
+    link.href = "https://www.twitch.tv/" + data.stream.channel.name;
+    link.target = "_blank";
+    link.className = "stream-link";
+
+
+    status.innerHTML = data.stream.channel.status.slice(0, 30) + "...";
+    status.className = "stream-description--online";
+    link.appendChild(status);
+    div.insertBefore(link, stream);
+  }
 }
+
+
 
 user.forEach(function(element) {
 
@@ -18,25 +40,7 @@ const finalUrl = url + element + urlEnd;
     .then(extractJson)
     .then(function(data) {
       console.log(data);
-      if (data.stream != null) {
-        console.log("online");
-        console.log(data.stream.channel.name);
-        let stream = document.getElementById(data.stream.channel.name);
-        stream.className = "stream-status--online";
-        stream.innerHTML = "Online";
-        let status = document.getElementById(data.stream.channel.name + "-stream");
-        status.innerHTML = data.stream.channel.status;
-
-        const link = document.createElement('a');
-        link.href = "https://www.twitch.tv/" + data.stream.channel.name;
-        link.target = "_blank";
-        const snippet = document.createTextNode("testing the link");
-        link.appendChild(snippet);
-        const cdnDiv = document.getElementById("cdnDiv")
-        cdnDiv.appendChild(link);
-        link.appendChild(status);
-
-      }
+      createLink(data);
     })
 
     .catch(function(error) {
