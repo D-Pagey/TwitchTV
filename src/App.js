9e25b29b9a0e7c673ee09c0eb7455e6396be5042
D-Pagey <dan.page91@gmail.com>
2018-04-08 00:34:01 +0100

    Adding Footer and Stream components.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Header from './components/Header';
+import Stream from './components/Stream';
+import Footer from './components/Footer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        <Header />  
+        <Stream />
+        <Footer />
       </div>
     );
   }
---
+++ src/components/Stream/Stream.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Stream.css';
+
+function Stream() {
+  return (
+    <div className='stream-container'>
+      <h3 className='stream-title'>FreeCodeCamp</h3>
+      <p className='stream-description'>Description</p>
+      <p className='stream-status'>Offline</p>
+    </div>
+    )
+}
+
+export default Stream;
