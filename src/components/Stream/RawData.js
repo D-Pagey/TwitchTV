de0d8596affe3d53b0329bbf6718075f18814030
D-Pagey <dan.page91@gmail.com>
2018-04-08 23:52:16 +0100

    Extracting raw data into separate file.

---
+++ src/components/Stream/RawData.js
@@ -0,0 +1,23 @@
+export default function rawData() {
+    const streamData = [
+    {
+      key: 1,
+      name: 'FreeCodeCamp',
+      description: 'Coding Stream',
+      status: 'Offline',
+    },
+    {
+      key: 2,
+      name: 'Macie Jay',
+      description: 'Rainbow 6 Stream',
+      status: 'Offline',
+    },
+    {
+      key: 3,
+      name: 'TSM Viss',
+      description: 'PUBG Stream',
+      status: 'Offline',
+    },
+  ];
+  return streamData;
+}
--- src/components/Stream/Stream.js
+++ src/components/Stream/Stream.js
@@ -1,15 +1,21 @@
 import React from 'react';
 
 import './Stream.css';
+import rawData from './RawData.js';
 
 function Stream() {
-  return (
-    <div className='stream-container'>
-      <h3 className='stream-title'>FreeCodeCamp</h3>
-      <p className='stream-description'>Description</p>
-      <p className='stream-status'>Offline</p>
+
+  let streamers;
+
+    streamers = rawData().map((element) => (
+      <div className='stream-container' key={element.key}>
+      <h3 className='stream-title'>{element.name}</h3>
+      <p className='stream-description'>{element.description}</p>
+      <p className='stream-status'>{element.status}</p>
     </div>
-    )
+    ))
+
+    return <main>{streamers}</main>;
 }
 
 export default Stream;
