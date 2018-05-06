2349a9d0b39c2b1043306d79d2e1cfd65a925b0c
D-Pagey <dan.page91@gmail.com>
2018-05-06 20:37:10 +0100

    Mapping over props object to display all streams.

--- src/components/Stream/Stream.js
+++ src/components/Stream/Stream.js
@@ -3,33 +3,22 @@ import React from 'react';
 import './Stream.css';
 
 function Stream({data}) {
-
-  console.log(data);
-
   return (
-
     <li className='stream-container'>
-    <h3 className='stream-title'>{data.name}</h3>
-       <p className='stream-description'>{data.description}</p>
-        <p className='stream-status'>{data.status}</p>
-     </li>
-
-    // <li className='stream-container'>
-    //     <h3 className='stream-title'>{data.streams.freecodecamp.name}</h3>
-    //   <p className='stream-description'>{data.streams.freecodecamp.description}</p>
-    //    <p className='stream-status'>{data.streams.freecodecamp.status}</p>
-    // </li>
+      <h3 className='stream-title'>{data.name}</h3>
+      <p className='stream-description'>{data.description}</p>
+      <p className='stream-status'>{data.status}</p>
+    </li>
   )
 }
 
 function StreamList({streams}) {
-
-  console.log(streams.freecodecamp);
+  const streamsArray = Object.keys(streams).map((i) => streams[i]);
+  const streamList = streamsArray.map((stream) => <Stream data={stream} />);
 
   return (
     <ul>
-      <Stream data={streams.freecodecamp}/>
-      <Stream data={streams.edberg}/>     
+      {streamList}
     </ul>
   )
 }
